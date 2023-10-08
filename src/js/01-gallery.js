a0f29d15c963c32b5455d48c836726dd4e3f09a9
Valerjan-1 <134737688+Valerjan-1@users.noreply.github.com>
2023-10-08 22:13:51 +0300

    add hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const list = document.querySelector(`.gallery`);
+const imgList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join(``);
+list.innerHTML = imgList;
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: false,
+  enableKeyboard: true,
+});
